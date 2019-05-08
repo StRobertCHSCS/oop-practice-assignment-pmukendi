a887eca8b307e79c5b67d2d4818ddfd5604b9b7e
pmukendi <swagoraptor909@gmail.com>
2019-05-07 21:20:56 -0400

    coin thing

--- Coin.py
+++ Coin.py
@@ -1,7 +1,29 @@
-# Define your Coin class here
+import random
 
+class Coin(object):
+
+  def __init__(self):
+    self.face = random.choice(["head", "tails"])
+
+  def get_face(self):
+    return self.face
+
+  def flip(self):
+
+    self.face = random.choice(["heads", "tails"])
 
 if __name__ == '__main__':
-  # put your code that utilizes your Coin class here
-  pass
-  
+
+  heads = 0
+  tails = 0
+
+  for i in range(1000):
+    myCoin = Coin()
+    myCoin.flip()
+
+    if myCoin.get_face() == "heads":
+      heads += 1
+    else:
+      tails += 1
+
+  print("total heads: " + str(heads) + "\nTotal tails: " + str(tails))
